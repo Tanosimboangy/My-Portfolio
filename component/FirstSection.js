32a9821b59e46c99058a6a9ce81568a90d601370
Tanosimboangy <jacquit.aja@onja.org>
2020-11-05 06:36:02 +0300

    adding class container to control the size of the viewport

--- component/FirstSection.js
+++ component/FirstSection.js
@@ -3,7 +3,9 @@ import React from "react";
 export default function FirstSection() {
     return (
         <section>
-            
+            <div className="container">
+
+            </div>
         </section>
     )
 }
--- component/Footer.js
+++ component/Footer.js
@@ -3,7 +3,9 @@ import React from "react";
 export default function Footer() {
     return (
         <footer>
-            <p>This is my new website that I have just built. It will not look goog but I am trying.</p>
+            <div className="container">
+                <p>This is my new website that I have just built. It will not look goog but I am trying.</p>
+            </div>
         </footer>
     )
 }
