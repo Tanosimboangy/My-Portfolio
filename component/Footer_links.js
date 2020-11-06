7b8bc7bf5e1f81f7429e642583536b5d50d9e2ea
Tanosimboangy <jacquit.aja@onja.org>
2020-11-06 13:40:32 +0300

    creating a type speed game

--- component/Footer_links.js
+++ component/Footer_links.js
@@ -6,29 +6,27 @@ import instagram from "../img/instagram.svg";
 
 export default function Footer_links() {
     return (
-        <>
-            <ul>
-                <li>
-                    <a href="http://facebook.com">
-                        <img src={facebook} />
-                    </a>
-                </li>
-                <li>
-                    <a href ="mailto:rak.hajasper@mail.com?">
-                        <img src={mail} />
-                    </a>
-                </li>
-                <li>
-                    <a href="http://twitter.com">
-                        <img src={twitter} />
-                    </a>
-                </li>
-                <li>
-                    <a href="http://instagram.com">
-                        <img src={instagram} />
-                    </a>
-                </li>
-            </ul>
-        </>
+        <ul>
+            <li>
+                <a href="http://facebook.com">
+                    <img src={facebook} />
+                </a>
+            </li>
+            <li>
+                <a href ="mailto:rak.hajasper@mail.com?">
+                    <img src={mail} />
+                </a>
+            </li>
+            <li>
+                <a href="http://twitter.com">
+                    <img src={twitter} />
+                </a>
+            </li>
+            <li>
+                <a href="http://instagram.com">
+                    <img src={instagram} />
+                </a>
+            </li>
+        </ul>
     )
 }
