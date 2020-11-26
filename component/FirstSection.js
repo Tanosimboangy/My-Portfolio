beb50f0802fda83d1261da7c4587d469128feb31
Tanosimboangy <jacquit.aja@onja.org>
2020-11-26 08:26:51 +0300

    fixing the part which contros the lyrics

--- component/FirstSection.js
+++ component/FirstSection.js
@@ -1,16 +1,33 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 export default function FirstSection() {
     return (
         <section className="first_section">
             <div className="container">
                 <div>
-                    <article>
-                        <h3>This is all about HTML and CSS</h3>
-                    </article>
-                    <article>
-                        <h3>This is all about Javascritp</h3>
-                    </article>
+                    <Link to="/html">
+                        <article className="html_article article_container">
+                            <button className="html_article--heading">HTML</button>
+                            <p className="html_article--paragraph">This will tell you more what I have done so far in HTML programming language.</p>
+                        </article>
+                    </Link>
+                    <Link to="/css">
+                        <article className="css_article article_container">
+                            <button>CSS</button>
+                            <p>CSS is a good language program to use to make your website beautiful.</p>
+                        </article>
+                    </Link>
+                    {/* <Link to="/jsvanilla">
+                        <article className="jsvanilla_article article_container">
+                            
+                        </article>
+                    </Link>
+                    <Link to="/react">
+                        <article className="react_article article_container">
+
+                        </article>
+                    </Link> */}
                 </div>
             </div>
         </section>
