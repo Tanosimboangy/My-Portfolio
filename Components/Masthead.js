e06784bc6fd6a0278165d2eecde8fb00dc84e92c
Tanosimboangy <jacquit.aja@onja.org>
2021-03-03 14:07:58 +0300

    adding some elements in the masthead component

--- Components/Masthead.js
+++ Components/Masthead.js
@@ -1,70 +1,69 @@
-import React from 'react'
+import React, {useState} from 'react'
 import jacquit from "../img/jacquit.jpg";
 import email from "../img/email.svg";
 import phone from "../img/phone.svg";
 
 function Blog() {
+    const [showPersonalDetails, setShowPersonalDetails] = useState(false)
     return (
-        <header className="masthead_container">
-            <div className="masthead_profile">
-                <img src={jacquit} alt="This is my image"/>
-            </div>
-            <div className="masthead_details">
-                <div className="personal_details">
-                    <ul className="personal_description">
-                        <li>
-                            <h2>Francois Jacquit</h2>
-                        </li>
-                        <li>
-                            <p>Front-end developer</p>
-                        </li>
-                    </ul>
-                    <ul className="personal_contact">
-                        <li className="email_contact">
-                            <a href ="mailto:rak.hajasper@mail.com?">
-                                <img src={email} alt="this is my email"/> Jacquit@gmail.com
-                            </a>
-                        </li>
-                        <li className="phonenumber_contact">
-                            <p>
-                                <img src={phone} alt="this is my phone number"/> (+261) 344605298
-                            </p>
-                        </li>
-                    </ul>
+        <header className="masthead">
+            <div className="masthead_container">
+                <div className="masthead_profile">
+                    <img className="image_profile" src={jacquit} alt="This_is_my_image"/>
                 </div>
-                <div className="personal_motivation"> 
-                    <p className="personal_motivation--text">
-                        Learning to write programs stretches your mind, and helps you think better, creates a way of thinking about things that I think is helpful in all domains.
+                <div className="masthead_details">
+                <div className="personal_details">
+                    <div className="personal_description">
+                        <h2 className="name">Francois Jacquit</h2>
+                        <p className="profession">Front-end developer</p>
+                    </div>
+                    <div className="personal_contact">
+                        <a className="email_contact" href ="mailto:rak.hajasper@mail.com?">
+                            <img src={email} alt="this is my email"/> Jacquit@gmail.com
+                        </a>
+                        <p className="phonenumber_contact">
+                            <img src={phone} alt="this is my phone number"/> (+261) 344605298
+                        </p>
+                    </div>
+                </div> 
+                <p className="personal_motivation">
+                    Learning to write programs stretches your mind, and helps you think better, creates a way of thinking about things that I think is helpful in all domains.
+                </p>
+                <button className="read_more">Do you want to read more</button>
+            </div>
+            </div>
+            {
+
+            }
+            {/* <div>
+                <div>
+                    <h2>About me</h2>
+                    <p>
+                        My name is jacquit and I'm 23 years old. I'm a student from Madagascar, more specificaly at Onja. I'm writing this blog because I want to share to you some of the great points that I've encountered in my study. Also, I want to get any suggestion from you for the things I still need to improve on this blog. So please have a great time to read this blog.
                     </p>
                 </div>
-                <button className="read_more">Do you want to read more</button>
-                {/* <div>
-                    <div>
-                        <p>
-                            My name is jacquit and I'm 23 years old. I'm a student from Madagascar, more specificaly at Onja. I'm writing this blog because I want to share to you some of the great points that I've encountered in my study. Also, I want to get any suggestion from you for the things I still need to improve on this blog. So please have a great time to read this blog.
-                        </p>
-                        <div>
-                            <h2>About ONJA</h2>
-                            <p>
-                                I'm studying at ONJA school, which is a NGO lead by SAM and located in Mahanoro. We are twenty students at ONJA, to learn english and coding with seven staff. This organisation is based on supporting the students who can't go to university but still want to study. We studyed english last year up to now, but this year we focus more on coding.
-                            </p>
-                        </div>
-                        <div>
-                            <h2>At the beginning of my study</h2>
-                            <p>
-                                Generally, I really love studying, because I want to learn many things. As far as I'm concerned, study assists me to have a better life in the future as many students want. That's what I'm doing now. However, at the beginning, I found it really hard to get started of my study at onja and to get use to it, but in fact working hard pushed me to get over that. That's why, I admit that nothing is easy on earth without trying hard. Apart from that, I had to change many things in my life, including the way of studying. Moreover, I needed to dicsipline myself and set up a new method of managing time so that I can use my time wisely.
-                            </p>
-                        </div>
-                        <div>
-                            <h2>At the moment</h2>
-                            <p>
-                                At this time, my study is getting better. Thanks to all sophisticated staff at ONJA, because without them I would never be on this stage now.
-                            </p>
-                        </div>
-                        <p>Thank you very much for devoting yourself to reading my blog.</p>
-                    </div>
-                </div> */}
-            </div>
+                <div>
+                    <h2>About ONJA</h2>
+                    <p>
+                        I'm studying at ONJA school, which is a NGO lead by SAM and located in Mahanoro. We are twenty students at ONJA, to learn english and coding with seven staff. This organisation is based on supporting the students who can't go to university but still want to study. We studyed english last year up to now, but this year we focus more on coding.
+                    </p>
+                </div>
+                <div>
+                    <h2>At the beginning of my study</h2>
+                    <p>
+                        Generally, I really love studying, because I want to learn many things. As far as I'm concerned, study assists me to have a better life in the future as many students want. That's what I'm doing now. However, at the beginning, I found it really hard to get started of my study at onja and to get use to it, but in fact working hard pushed me to get over that. That's why, I admit that nothing is easy on earth without trying hard. Apart from that, I had to change many things in my life, including the way of studying. Moreover, I needed to dicsipline myself and set up a new method of managing time so that I can use my time wisely.
+                    </p>
+                </div>
+                <div>
+                    <h2>At the moment</h2>
+                    <p>
+                        At this time, my study is getting better. Thanks to all sophisticated staff at ONJA, because without them I would never be on this stage now.
+                    </p>
+                </div>
+                <div>
+                    <p>Thank you very much for devoting yourself to reading my blog.</p>
+                </div>
+            </div> */}
         </header>
     )
 }
--- dist/My_Portfolio.e31bb0bc.js
+++ dist/My_Portfolio.e31bb0bc.js
@@ -29847,7 +29847,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
-var _react = _interopRequireDefault(require("react"));
+var _react = _interopRequireWildcard(require("react"));
 
 var _jacquit = _interopRequireDefault(require("../img/jacquit.jpg"));
 
@@ -29857,41 +29857,66 @@ var _phone = _interopRequireDefault(require("../img/phone.svg"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function () { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
 function Blog() {
+  var _useState = (0, _react.useState)(false),
+      _useState2 = _slicedToArray(_useState, 2),
+      showPersonalDetails = _useState2[0],
+      setShowPersonalDetails = _useState2[1];
+
   return /*#__PURE__*/_react.default.createElement("header", {
+    className: "masthead"
+  }, /*#__PURE__*/_react.default.createElement("div", {
     className: "masthead_container"
   }, /*#__PURE__*/_react.default.createElement("div", {
     className: "masthead_profile"
   }, /*#__PURE__*/_react.default.createElement("img", {
+    className: "image_profile",
     src: _jacquit.default,
-    alt: "This is my image"
+    alt: "This_is_my_image"
   })), /*#__PURE__*/_react.default.createElement("div", {
     className: "masthead_details"
   }, /*#__PURE__*/_react.default.createElement("div", {
     className: "personal_details"
-  }, /*#__PURE__*/_react.default.createElement("ul", {
+  }, /*#__PURE__*/_react.default.createElement("div", {
     className: "personal_description"
-  }, /*#__PURE__*/_react.default.createElement("li", null, /*#__PURE__*/_react.default.createElement("h2", null, "Francois Jacquit")), /*#__PURE__*/_react.default.createElement("li", null, /*#__PURE__*/_react.default.createElement("p", null, "Front-end developer"))), /*#__PURE__*/_react.default.createElement("ul", {
+  }, /*#__PURE__*/_react.default.createElement("h2", {
+    className: "name"
+  }, "Francois Jacquit"), /*#__PURE__*/_react.default.createElement("p", {
+    className: "profession"
+  }, "Front-end developer")), /*#__PURE__*/_react.default.createElement("div", {
     className: "personal_contact"
-  }, /*#__PURE__*/_react.default.createElement("li", {
-    className: "email_contact"
   }, /*#__PURE__*/_react.default.createElement("a", {
+    className: "email_contact",
     href: "mailto:rak.hajasper@mail.com?"
   }, /*#__PURE__*/_react.default.createElement("img", {
     src: _email.default,
     alt: "this is my email"
-  }), " Jacquit@gmail.com")), /*#__PURE__*/_react.default.createElement("li", {
+  }), " Jacquit@gmail.com"), /*#__PURE__*/_react.default.createElement("p", {
     className: "phonenumber_contact"
-  }, /*#__PURE__*/_react.default.createElement("p", null, /*#__PURE__*/_react.default.createElement("img", {
+  }, /*#__PURE__*/_react.default.createElement("img", {
     src: _phone.default,
     alt: "this is my phone number"
-  }), " (+261) 344605298")))), /*#__PURE__*/_react.default.createElement("div", {
+  }), " (+261) 344605298"))), /*#__PURE__*/_react.default.createElement("p", {
     className: "personal_motivation"
-  }, /*#__PURE__*/_react.default.createElement("p", {
-    className: "personal_motivation--text"
-  }, "Learning to write programs stretches your mind, and helps you think better, creates a way of thinking about things that I think is helpful in all domains.")), /*#__PURE__*/_react.default.createElement("button", {
+  }, "Learning to write programs stretches your mind, and helps you think better, creates a way of thinking about things that I think is helpful in all domains."), /*#__PURE__*/_react.default.createElement("button", {
     className: "read_more"
-  }, "Do you want to read more")));
+  }, "Do you want to read more"))));
 }
 
 var _default = Blog;
@@ -30144,7 +30169,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54101" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55893" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
